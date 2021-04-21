5f56e7ee3b585e9ddbe14c141f1be2393a031d25
YOKI1216 <1151509140@qq.com>
2021-04-21 20:59:31 +0800

    默认的

--- src/cs/edu/gitsub.java
+++ src/cs/edu/gitsub.java
@@ -3,7 +3,7 @@ package cs.edu;
 public class gitsub {
     public static void main(String[] args) {
         System.out.println("success");
-        System.out.println("change");
+
         System.out.println("完成学习");
     }
 }
